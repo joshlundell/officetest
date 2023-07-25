7e70a184d181551492fdd91e2fd95ec1e9c2963b
joshlundell <140119328+joshlundell@users.noreply.github.com>
2023-07-25 16:21:37 -0500

    Update grammarly.sh

--- grammarly.sh
+++ grammarly.sh
@@ -1,58 +1,42 @@
-#!/bin/sh
-currentUser=$(ls -l /dev/console | awk '{ print $3 }')
-dmgfile="Grammarly.dmg"
-volname="Grammarly"
-/bin/echo "--"
-# check if logged in user has admin rights
-IsUserAdmin=$(id -G $currentUser| grep -v 80)
-if [ -n "$IsUserAdmin" ]; then
-grammarly_dir="/Users/${currentUser}/Applications"
-    /bin/echo "`date`: $currentUser is not local admin"
+#!/bin/bash
+
+# Grammarly DMG URL
+GRAMMARLY_DMG_URL="https://download-mac.grammarly.com/Grammarly.dmg"
+
+# Download the Grammarly DMG file
+echo "Downloading Grammarly DMG..."
+curl -O "$GRAMMARLY_DMG_URL"
+
+# Mount the DMG file
+echo "Mounting Grammarly DMG..."
+hdiutil attach grammarly.dmg
+
+# Locate the Grammarly Installer and run it
+echo "Installing Grammarly..."
+installer_path=$(find /Volumes/Grammarly -name "Grammarly Installer.app" -type d -maxdepth 1)
+if [[ -n "$installer_path" ]]; then
+    "$installer_path/Contents/MacOS/Grammarly Installer" -silent
 else
-grammarly_dir="/Applications"
-    /bin/echo "`date`: $currentUser is a local admin"
+    echo "Grammarly Installer not found on the mounted volume."
 fi
-# check if Applications folder exists
-if [ -d "$grammarly_dir" ]; then
-echo "Application folder exists"
-else
-echo "Application folder doesn't exist"
-mkdir "$grammarly_dir"
-    chown -R "$currentUser":staff "${grammarly_dir}"
-fi
-# check Download link and availability of Grammarly CDN
-if [ -z "$4" ]; then
-    url='https://download-mac.grammarly.com/Grammarly.dmg'
-    /bin/echo "No arguments supplied. Using default address"
-else
-    url=$4
-fi
-/usr/bin/curl -f -s -I $url &>/dev/null
-if [[ $? -eq 0 ]]; then
-    /bin/echo "`date`: Grammarly Desktop download site ${url} is available."
-else
-    /bin/echo "`date`: Grammarly Desktop download site ${url} is NOT available."
-    exit 1
-fi
-/bin/echo "`date`: Checking and unmounting existing installer disk image"
-/usr/bin/hdiutil detach $(/bin/df | /usr/bin/grep "${volname}" | awk '{print $1}') -quiet
-/bin/sleep 10
-/bin/echo "`date`: Downloading latest version"
-/usr/bin/curl -s -o /tmp/${dmgfile} ${url}
-/bin/echo "`date`: Mounting installer disk image"
-/usr/bin/hdiutil attach /tmp/${dmgfile} -nobrowse -quiet
-/bin/echo "`date`: Installing..."
-/bin/echo "${grammarly_dir}/Grammarly Desktop.app"
-/usr/bin/sudo -u "$currentUser" cp -R "/Volumes/${volname}/Grammarly Installer.app" "${grammarly_dir}/Grammarly Desktop.app"
-/usr/sbin/chown -R "$currentUser":staff "${grammarly_dir}/Grammarly Desktop.app"
-xattr -r -d com.apple.quarantine "${grammarly_dir}/Grammarly Desktop.app"
-/bin/sleep 10
-/bin/echo "`date`: Unmounting installer disk image"
-/usr/bin/hdiutil detach $(/bin/df | /usr/bin/grep "${volname}" | awk '{print $1}') -quiet
-/bin/sleep 10
-/bin/echo "`date`: Deleting disk image"
-/bin/rm /tmp/"${dmgfile}"
-/bin/echo "`date`: Setting onboarding deferral"
-/usr/bin/sudo -u ${currentUser} defaults write com.grammarly.ProjectLlama deferOnboarding -bool YES
-/bin/echo "`date`: Running the app"
-/usr/bin/sudo -u ${currentUser} open "${grammarly_dir}/Grammarly Desktop.app" --args launchSourceInstaller
+
+# Clean up - unmount the DMG
+echo "Cleaning up..."
+hdiutil detach /Volumes/Grammarly
+
+# Remove the downloaded DMG
+rm grammarly.dmg
+
+# Set permissions for Grammarly
+echo "Setting permissions for Grammarly..."
+# Replace the paths below with the actual paths where Grammarly is installed.
+grammarly_app_path="/Applications/Grammarly.app"
+grammarly_app_support_path="/Library/Application Support/Grammarly"
+
+# Set permissions for the Grammarly app
+chmod -R 755 "$grammarly_app_path"
+
+# Set permissions for the Grammarly Application Support directory
+chmod -R 755 "$grammarly_app_support_path"
+
+echo "Grammarly installation completed."
